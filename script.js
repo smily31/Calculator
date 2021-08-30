9a8edc62631d883031e938eb1021afdec65f177c
Ritika Bhatt <ritikabhatt3110@gmail.com>
2021-08-30 15:22:11 +0530

    logic added

---
+++ script.js
@@ -0,0 +1,37 @@
+//   getting the screen to show numbers and results
+  let screen = document.getElementById("screen");
+
+//   getting all buttons in buttons array
+  let buttons = document.getElementsByClassName('btn');
+
+//   variable to store expression to get evaluate
+  let equation = "";
+
+for(button of buttons)
+{
+    button.addEventListener('click' , (e) => 
+    {
+        buttonText = e.target.innerText;
+        // console.log(buttonText);
+        if(buttonText == 'C')
+        {
+            screen.value = "";
+        }
+        else if(buttonText == 'Esc')
+        {
+            equation = equation.substr(0, equation.length-1);
+            screen.value = equation;
+        }
+        else if(buttonText == '=')
+        {
+            // eval is a javascript function to perform computation
+            screen.value = eval(equation);
+            equation = "";
+        }
+        else 
+        {
+            equation += buttonText;
+            screen.value = equation;
+        }
+    })
+}
